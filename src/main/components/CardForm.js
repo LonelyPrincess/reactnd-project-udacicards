9623e0c54eab2ed6c7d6c91cfa35f7e4962c5909
Sara <sara.her.su@gmail.com>
2017-12-22 10:24:49 +0000

    feat: Card form validation
    Before adding a new card to a deck, the app does now check if the
    following criteria are met:
    
     - The user has writen a question.
     - The user has filled, at least, 2 of the 3 possible answers to the
       question.
     - The user has chosen a non-empty answer field as the right answer.
    
    Only when the form values match these three conditions, the add card to
    deck action is dispatched.

--- src/main/components/CardForm.js
+++ src/main/components/CardForm.js
@@ -30,16 +30,53 @@ class CardForm extends React.Component {
     this.setState({ validAnswer: answerIndex });
   };
 
-  //
-  submitCard = () => {
-    // TODO: validation
+  // Validate that we have the minimal required data before adding card to the deck
+  validateCard = () => {
+    const { question, answers, validAnswer } = this.state;
 
+    if (!question) {
+      displayToast('You need to write a question.');
+      this.refs.inputQuestion.focus();
+      return false;
+    }
+
+    if (answers.filter(answer => answer).length < 2) {
+      displayToast('You need to fill, at least, two answers for your question.');
+      for (let i = 0; i < answers.length; i++) {
+        if (!answers[i]) {
+          this.refs[`inputAnswer${i + 1}`].focus();
+          break;
+        }
+      }
+      return false;
+    }
+
+    if (!answers[validAnswer]) {
+      displayToast('The right answer cannot be empty.');
+      this.refs[`inputAnswer${validAnswer + 1}`].focus();
+      return false;
+    }
+
+    return true;
+  };
+
+  // If form passes validation, add card to deck
+  submitCard = () => {
+
+    if (!this.validateCard()) {
+      console.log('Card form validation failed!' );
+      return;
+    }
+
+    const { question, answers, validAnswer } = this.state;
+
+    // Add card to the deck
     const card = {
-      text: this.state.question,
-      answers: this.state.answers
+      text: question,
+      answers: answers
         .map((answer, index) => ({
           text: answer,
-          isTrue: this.state.validAnswer === index
+          isTrue: validAnswer === index
         }))
         .filter(answer => answer.text)     // Filter empty answers
     };
@@ -55,14 +92,18 @@ class CardForm extends React.Component {
     return (
       <View>
         <Text>Create a new card for current deck:</Text>
-        <TextInput value={this.state.question} placeholder="Question"
+        <TextInput
+          ref="inputQuestion"
+          value={this.state.question} placeholder="Question"
           onChangeText={(question) => this.setState({ question })} />
 
         <Text>Write possible answers for your question (at least 2):</Text>
 
         {this.state.answers.map((item, index) => (
           <View key={index}>
-            <TextInput value={this.state.answers[index]} placeholder={`Answer ${index + 1}`}
+            <TextInput
+              ref={`inputAnswer${index + 1}`}
+              value={this.state.answers[index]} placeholder={`Answer ${index + 1}`}
               onChangeText={(answer) => {
                 const updatedAnswers = this.state.answers;
                 updatedAnswers[index] = answer;
