7c5c2ba782024bf559d90b7a3990991493fe6c71
Sara <sara.her.su@gmail.com>
2017-12-22 11:06:22 +0000

    feat: Visual feedback on quiz results
    The quiz results will now include a message and an emoticon depending on
    the obtained score.

--- src/main/components/QuizResults.js
+++ src/main/components/QuizResults.js
@@ -4,15 +4,36 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function QuizResults ({ successRatio, onReplayClick, onBackClick }) {
+  let icon, message;
+
+  if (successRatio < 40) {
+    icon = 'emoticon-dead';
+    message = `Oops! It seems as if you need to review more...`;
+  } else if (successRatio < 50) {
+    icon = 'emoticon-sad';
+    message = `Almost there! You may make it next time!`;
+  } else if (successRatio < 70) {
+    icon = 'emoticon-happy';
+    message = `Hey, you made it! Good job!`;
+  } else if (successRatio < 90) {
+    icon = 'emoticon-excited';
+    message = `Congratulations! You're on the way to become a master on this!`;
+  } else {
+    icon = 'emoticon-cool';
+    message = `Wow! You sure are a master on this topic!`;
+  }
+
   return (
     <View>
       <Text>Obtained score</Text>
       <Text>{successRatio}%</Text>
+      <MaterialCommunityIcons name={icon} size={200} />
+      <Text>{message}</Text>
       <TouchableOpacity onPress={onReplayClick}>
         <MaterialCommunityIcons name="replay" size={50} />
       </TouchableOpacity>
       <TouchableOpacity onPress={onBackClick}>
-        <MaterialCommunityIcons name="cards" size={50} />
+        <MaterialCommunityIcons name="exit-to-app" size={50} />
       </TouchableOpacity>
     </View>
   );
