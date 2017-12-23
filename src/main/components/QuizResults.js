458f7f3311c6e61cc8f25bcbb4d1cc447716a573
Sara <sara.her.su@gmail.com>
2017-12-23 12:30:28 +0000

    design: Styles added to quiz results
    Some temporary styles added to the quiz results screen. They're mostly
    related to the structure of all of its child elements on screen. The color
    set is highly likely to be changed.

--- src/main/components/QuizResults.js
+++ src/main/components/QuizResults.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { NavigationActions } from 'react-navigation';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { white, green, orange, red } from '../utils/Colors';
+import Button from './Button';
 
 export default function QuizResults ({ successRatio, onReplayClick, onBackClick }) {
   let icon, message;
@@ -24,17 +27,51 @@ export default function QuizResults ({ successRatio, onReplayClick, onBackClick 
   }
 
   return (
-    <View>
-      <Text>Obtained score</Text>
-      <Text>{successRatio}%</Text>
-      <MaterialCommunityIcons name={icon} size={200} />
-      <Text>{message}</Text>
-      <TouchableOpacity onPress={onReplayClick}>
-        <MaterialCommunityIcons name="replay" size={50} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onBackClick}>
-        <MaterialCommunityIcons name="exit-to-app" size={50} />
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <Text style={styles.scoreTitle}>Your score is...</Text>
+      <Text style={styles.score}>{successRatio}%</Text>
+      <Text style={styles.feedbackMessage}>{message}</Text>
+      <MaterialCommunityIcons name={icon} size={200} style={styles.icon}/>
+      <View style={styles.buttonContainer}>
+        <Button onPress={onReplayClick}>
+          <MaterialCommunityIcons name="replay" size={50} />
+        </Button>
+        <Button onPress={onBackClick} style={{ backgroundColor: red }}>
+          <MaterialCommunityIcons name="exit-to-app" size={50} />
+        </Button>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: white
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    justifyContent: 'space-around'
+  },
+  icon: {
+    color: orange,
+    marginTop: 10,
+    marginBottom: 25
+  },
+  score: {
+    fontSize: 36,
+    color: green,
+    marginBottom: 10,
+    fontWeight: 'bold'
+  },
+  scoreTitle: {
+    fontSize: 16,
+    color: green
+  },
+  feedbackMessage: {
+    textAlign: 'center'
+  }
+});
