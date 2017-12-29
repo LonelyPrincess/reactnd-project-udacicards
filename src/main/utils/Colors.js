1f8c08c4bb6bb3fcde3b04246eeeaf22734e4ce3
Sara <sara.her.su@gmail.com>
2017-12-29 13:50:25 +0000

    design: Red color updated

--- src/main/utils/Colors.js
+++ src/main/utils/Colors.js
@@ -4,7 +4,7 @@ export const gray = '#666666';
 export const lightGreen = '#e2f1e2';
 export const green = '#8cc63f';
 export const orange = '#ff9800';
-export const red = '#be1e2e';
+export const red = '#f44336';
 
 // TODO: remove?
 export const brown = '#f0dfd2';
