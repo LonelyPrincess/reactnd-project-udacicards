bd455fa4bcd9464c3f78d1d894dff85d14301076
Sara <sara.her.su@gmail.com>
2018-01-22 13:48:11 +0000

    refactor: New constants file with screen keys

---
+++ src/main/constants/Screens.js
@@ -0,0 +1,7 @@
+export const MAIN = 'Home';
+export const DECK_LIST = 'DeckList';
+export const DECK_FORM = 'CreateDeck'
+export const DECK_DETAILS = 'DeckDetails';
+export const CARD_FORM = 'CreateCard';
+export const QUIZ = 'DeckQuiz';
+export const QUIZ_RESULTS = 'QuizResults';
